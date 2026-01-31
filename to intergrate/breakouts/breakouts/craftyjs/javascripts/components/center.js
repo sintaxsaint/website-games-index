f209f88f492f4d2a58aa6295adc148c8984c7d73
ghostrrr <ereadman2013@gmail.com>
2026-01-31 22:05:58 +0000

    r

--- to intergrate/breakouts/breakouts/craftyjs/javascripts/components/center.js
+++
@@ -1,8 +0,0 @@
-Crafty.c('Center', {
-	init: function() {
-		this.attr({
-			x: Crafty.stage.elem.clientWidth / 2 - this.w / 2,
-			y: Crafty.stage.elem.clientHeight / 2 - this.h / 2
-		});
-	}
-});
--- to intergrate/t-rex-runner/README.md
+++
@@ -1,3 +0,0 @@
-the trex runner game abstracted from chrome offline err page.
-
-![chrome offline game cast](img/chrome_offline_game.gif)
